7350811e3bc236c5417a1f8a0a216f9d2d594904
phuongnguyen <phuongnguyen@futurify.vn>
2017-04-25 18:00:37 +0700

    Initial commit

---
+++ AngularJSTraining/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
+
+namespace AngularJSTraining
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            var trackPackageRouteHandler = new RouteHandler(context =>
+            {
+                var file = env.WebRootFileProvider.GetFileInfo("index.html");
+                return context.Response.SendFileAsync(file);
+            });
+
+            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+            routeBuilder.MapRoute(
+             "SPA",
+             @"{*url:regex(^(?!api/).*(?<!\..*)$)}");
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+        }
+    }
+}
---
+++ AngularJSTraining/wwwroot/app/blank.app.module.js
@@ -0,0 +1,12 @@
+﻿angular.module('ngTraining', [
+    
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+
+    'ui.bootstrap'
+
+]);
